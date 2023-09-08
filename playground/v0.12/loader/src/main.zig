cbc8825c04aaf9d5fa776e8c3854389636575053
xkyii <xkyii@foxmail.com>
2023-09-08 10:45:03 +0800

    Up: load ok

--- playground/v0.12/loader/src/main.zig
+++ playground/v0.12/loader/src/main.zig
@@ -1,24 +1,14 @@
 const std = @import("std");
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    const dllpath =
+        // 绝对路径
+        // \\E:\xk\Code\xkyss\xkyss.zig\playground\v0.12\dll\main.dll
+        // 相对路径
+        \\../dll/main.dll
+    ;
+    std.debug.print("Dll Path: {s}\n", .{dllpath});
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // don't forget to flush!
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+    // 加载dll
+    _ = try std.DynLib.open(dllpath);
 }
