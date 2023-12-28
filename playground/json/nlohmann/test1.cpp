54a45a668e43b68fabec0dc72d2786d084f8ca07
xkyii@th <xkyii@foxmail.com>
2023-12-28 14:06:58 +0800

    Up: 测试nlohmann

--- playground/json/nlohmann/test1.cpp
+++ playground/json/nlohmann/test1.cpp
@@ -1,13 +1,11 @@
-﻿// main.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
-
+﻿
 #include <iostream>
-#include <assert.h>
 #include <nlohmann/json.hpp>
+#include <catch2/catch_test_macros.hpp>
 
 using json = nlohmann::json;
 
-namespace ns {
+namespace test1 {
     // a simple struct to model a person
     struct person {
         std::string name;
@@ -26,10 +24,10 @@ namespace ns {
     }
 }
 
-int main()
+TEST_CASE("定义to_json/from_json")
 {
     // create a person
-    ns::person p{ "Ned Flanders", "744 Evergreen Terrace", 60 };
+    test1::person p{ "Ned Flanders Test1", "744 Evergreen Terrace", 60 };
 
     // conversion: person -> json
     json j = p;
@@ -38,9 +36,9 @@ int main()
     // {"address":"744 Evergreen Terrace","age":60,"name":"Ned Flanders"}
 
     // conversion: json -> person
-    auto p2 = j.get<ns::person>();
+    auto p2 = j.get<test1::person>();
 
-    assert(p.name == p2.name);
-    assert(p.age == p2.age);
-    assert(p.address == p2.address);
+    REQUIRE(p.name == p2.name);
+    REQUIRE(p.age == p2.age);
+    REQUIRE(p.address == p2.address);
 };
