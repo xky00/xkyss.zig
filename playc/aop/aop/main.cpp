2d6612558afba527fe9a117414aeb76e7c1eeaef
xkyii@th <xkyii@foxmail.com>
2024-05-11 11:14:37 +0800

    Up: 使用代理的方式

--- playc/aop/aop/main.cpp
+++ playc/aop/aop/main.cpp
@@ -4,34 +4,34 @@
 #include "Service.h"
 
 
-// 通用的日志记录函数
-void log(const std::string& message) {
-    std::cout << "Logging: " << message << std::endl;
-}
+class ServiceProxy : public Service {
+public:
+    // 添加用户
+    void addUser(const User& user) {
+        // 在调用原始Service对象之前添加额外逻辑
+        std::cout << "Adding user: " << user.getName() << std::endl;
+        Service::addUser(user);
+    }
 
-// AOP函数，接受原始函数和日志函数，返回一个新的函数对象
-auto addLogging(const std::function<User(const std::string&)>& originalFunction, const std::string& message) {
-    return [=](const std::string& name) {
-        log(message);
-        return originalFunction(name);
-        };
-}
-
+    // 根据姓名获取用户
+    User getUser(const std::string& name) const {
+        // 在调用原始Service对象之前添加额外逻辑
+        std::cout << "Getting user: " << name << std::endl;
+        return Service::getUser(name);
+    }
+};
 
 int main() {
-    // 创建原始的Service对象
-    Service service;
+    // 创建Service代理对象
+    ServiceProxy serviceProxy;
 
     // 添加一些用户到Service
-    service.addUser(User("John", 1234));
-    service.addUser(User("Alice", 5678));
-
-    // 使用AOP增加日志记录功能
-    auto getUserWithLogging = addLogging(std::bind(&Service::getUser, service, std::placeholders::_1), "Before calling getUser for name");
+    serviceProxy.addUser(User("John", 1234));
+    serviceProxy.addUser(User("Alice", 5678));
 
     // 根据姓名获取用户
     std::string searchName = "Alice";
-    User foundUser = getUserWithLogging(searchName);
+    User foundUser = serviceProxy.getUser(searchName);
 
     if (foundUser.getName() != "") {
         std::cout << "User found: " << foundUser.getName() << ", Code: " << foundUser.getCode() << std::endl;
