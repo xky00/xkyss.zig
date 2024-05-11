ad3b0cca24b0ebadfa14d655d271ece35e8b10ae
xkyii@th <xkyii@foxmail.com>
2024-05-11 14:13:57 +0800

    Up: 优化

--- playc/aop/aop/main.cpp
+++ playc/aop/aop/main.cpp
@@ -5,34 +5,32 @@
 
 
 #define GENERATE_PROXY_CACHE_CLASS_BEGIN(TOriginal) \
-    class TOriginal##Proxy : public TOriginal { 
+    struct TOriginal##Proxy : public TOriginal { \
+        using Original = TOriginal;
 
 #define GENERATE_PROXY_CACHE_CLASS_END() }
 
-#define GENERATE_PROXY_CACHE_GETTER0(TOriginal, MethodName, TReture) \
-    public: \
-    TReture MethodName() { \
+#define GENERATE_PROXY_CACHE_GETTER0(MethodName, TReturn) \
+    TReturn MethodName() { \
             std::cout << "Aspect: " << #MethodName  << std::endl; \
-            return TOriginal::MethodName(); \
+            return Original::MethodName(); \
     }
 
-#define GENERATE_PROXY_CACHE_GETTER1(TOriginal, MethodName, TReture, TParam1) \
-    public: \
-    TReture MethodName(TParam1 param1) { \
+#define GENERATE_PROXY_CACHE_GETTER1(MethodName, TReturn, TParam1) \
+    TReturn MethodName(TParam1 param1) { \
             std::cout << "Aspect: " << #MethodName << "(" << param1 << ")" << std::endl; \
-            return TOriginal::MethodName(param1); \
+            return Original::MethodName(param1); \
     }
 
-#define GENERATE_PROXY_CACHE_GETTER2(TOriginal, MethodName, TReture, TParam1, TParam2) \
-    public: \
-    TReture MethodName(TParam1 param1, TParam2 param2) { \
+#define GENERATE_PROXY_CACHE_GETTER2(MethodName, TReturn, TParam1, TParam2) \
+    TReturn MethodName(TParam1 param1, TParam2 param2) { \
             std::cout << "Aspect: " << #MethodName << "(" << param1 << ", " << param2 << ")" << std::endl; \
-            return TOriginal::MethodName(param1, param2); \
+            return Original::MethodName(param1, param2); \
     }
 
 GENERATE_PROXY_CACHE_CLASS_BEGIN(Service)
-    GENERATE_PROXY_CACHE_GETTER1(Service, getUser, User, const std::string&)
-    GENERATE_PROXY_CACHE_GETTER2(Service, getUser2, User, const std::string&, const int)
+    GENERATE_PROXY_CACHE_GETTER1(getUser, User, const std::string&)
+    GENERATE_PROXY_CACHE_GETTER2(getUser2, User, const std::string&, const int)
 GENERATE_PROXY_CACHE_CLASS_END();
 
 #define COUNT(...) std::tuple_size<decltype(std::make_tuple(__VA_ARGS__))>::value
