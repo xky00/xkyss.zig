9a24c951462eb20281c601bb452f929a43aad483
xkyii@th <xkyii@foxmail.com>
2024-05-14 18:39:48 +0800

    Up: 字符串指针转换

--- xkyss-core/src/core/VoidPoint.zig
+++ xkyss-core/src/core/VoidPoint.zig
@@ -3,16 +3,29 @@
 
 const std = @import("std");
 
-test "to anyopaque" {
+test "u32 with anyopaque" {
     var x: u32 = 111;
     const p: *const anyopaque = @ptrCast(&x);
     const r: *const u32 = @ptrCast(@alignCast(p));
 
-    std.debug.print("\n", .{});
+    std.debug.print("\n\n", .{});
     std.debug.print("x: {}\n", .{x});
-    std.debug.print("p: {}\n", .{p});
+    std.debug.print("p: {x}\n", .{p});
     std.debug.print("r: {}\n", .{r});
     std.debug.print("v: {}\n", .{r.*});
 
     try std.testing.expectEqual(x, r.*);
+}
+
+test "string with anyopaque" {
+    const s = "hello";
+    const p: *const anyopaque = @ptrCast(&s);
+    const r: *const *const [5:0]u8 = @ptrCast(@alignCast(p));
+
+    std.debug.print("\n\n", .{});
+    std.debug.print("s: {s}, len: {d}, type: {}\n", .{ s, s.len, @TypeOf(s) });
+    std.debug.print("ps: {}\n", .{&s});
+    std.debug.print("p: {}\n", .{p});
+    std.debug.print("r: {}\n", .{r});
+    std.debug.print("v: {s}\n", .{r.*});
 }
