fd4542f6e5badafbee59ee9f4af024aece365a87
xkyii <xkyii@foxmail.com>
2024-05-22 21:15:23 +0800

    Up: handle timer

--- xkyss-core/src/event/Loop.zig
+++ xkyss-core/src/event/Loop.zig
@@ -76,13 +76,15 @@ pub fn run(self: *Self) !i32 {
     // 轮询
     // for (0..10) |i| {
     while (self.status != Status.stop) {
+        const delta = Time.gethrtime() - self.current_time;
         self.current_time = Time.gethrtime();
         // std.debug.print("loop {} at {}\n", .{ i, self.current_time });
+        std.debug.print("pause_sleep_time: {}, delta: {}\n", .{ pause_sleep_time, delta });
         std.debug.print("loop {} at {}\n", .{ self.loop_count, self.current_time });
 
         if (self.status == Status.pause) {
             Time.sleep(pause_sleep_time);
-            // continue;
+            continue;
         }
         self.loop_count += 1;
         // // timers -> events -> idles
@@ -91,6 +93,11 @@ pub fn run(self: *Self) !i32 {
         // if (loop->timers.size() != 0) {
         //     ntimer = hloop_handle_timers(loop);
         //     event_timeout = MAX(MIN_EVENT_TIMEOUT, loop->min_timer_timeout/10);
+        // }
+        // var nidle: u32 = 0;
+        // var ntimer: u32 = 0;
+        // if (self.timers.count() != 0) {
+        //     ntimer = try self.handle_timers();
         // }
         // if (loop->events.size() == 0 || loop->idles.size() != 0) {
         //     event_timeout = MIN(event_timeout, MAX_EVENT_TIMEOUT);
@@ -103,8 +110,10 @@ pub fn run(self: *Self) !i32 {
         Time.sleep(pause_sleep_time);
         // }
         // if (ntimer == 0 && nevent == 0 && loop->idles.size() != 0) {
-        const nidle = try self.handle_idles();
-        std.debug.print("\tnidle: {}\n", .{nidle});
+        // if (ntimer == 0 and self.idles.count() != 0) {
+        //     nidle = try self.handle_idles();
+        // }
+        // std.debug.print("\tnidle: {}, ntimer: {}\n", .{ nidle, ntimer });
         // }
     }
 
@@ -214,6 +223,7 @@ pub fn add_timer(self: *Self, callback: *const fn (*Timer, *void) void, userdata
     timer.callback = callback;
     timer.userdata = userdata;
     timer.timeout = timeout;
+    timer.next_timeout = Time.gethrtime() + (timeout * 1000);
     timer.repeat = repeat;
     try self.timers.put(timer.id, timer);
     return timer;
@@ -234,6 +244,36 @@ pub fn del_timer(self: *Self, timer_id: u32) bool {
     }
 }
 
+pub fn handle_timers(self: *Self) !u32 {
+    std.debug.print("handle_timers\n", .{});
+
+    var ntimer: u32 = 0;
+
+    var it = self.timers.valueIterator();
+    while (it.next()) |ptimer| {
+        const timer = ptimer.*;
+        if (timer.destroy or timer.repeat == 0) {
+            _ = self.timers.remove(timer.id);
+            self.allocator.destroy(timer);
+            continue;
+        }
+        if (timer.disable) {
+            continue;
+        }
+        std.debug.print("timeout: {}, next_timeout: {}, current_time: {}\n", .{ timer.timeout, timer.next_timeout, self.current_time });
+        if (timer.next_timeout < self.current_time) {
+            ntimer += 1;
+            timer.callback(timer, timer.userdata);
+            timer.next_timeout += timer.timeout * 1000;
+            if (timer.repeat != 0xFFFFFFFF) {
+                timer.repeat -= 1;
+            }
+        }
+    }
+
+    return ntimer;
+}
+
 fn stop_async(self: *Self) !void {
     _ = try std.Thread.spawn(.{}, (struct {
         fn runner(loop: *Self) void {
@@ -244,95 +284,107 @@ fn stop_async(self: *Self) !void {
     }).runner, .{self});
 }
 
-test "init & deinit" {
-    var loop = Self.init(std.testing.allocator);
-    defer loop.deinit();
+// test "init & deinit" {
+//     var loop = Self.init(std.testing.allocator);
+//     defer loop.deinit();
+// }
+
+// test "run" {
+//     std.debug.print("ignore\n", .{});
+//     // var loop = Self{};
+//     // _ = run(&loop);
+// }
+
+// test "stop" {
+//     var loop = Self.init(std.testing.allocator);
+//     defer loop.deinit();
+//     _ = stop(&loop);
+// }
+
+// test "pause" {
+//     var loop = Self.init(std.testing.allocator);
+//     defer loop.deinit();
+//     _ = pause(&loop);
+// }
+
+// test "resume" {
+//     var loop = Self.init(std.testing.allocator);
+//     defer loop.deinit();
+//     _ = unpause(&loop);
+// }
+
+// fn test_idle_cb(idle: *Idle, ud: *void) void {
+//     std.debug.print("test_cb: idle: {}, ud: {}", .{ idle, ud });
+// }
+
+// test "add_idle" {
+//     var loop = Self.init(std.testing.allocator);
+//     defer loop.deinit();
+//     var x: u32 = 99;
+//     const ud: *void = @ptrCast(&x);
+//     const idle = try loop.add_idle(&test_idle_cb, ud, 5);
+//     std.debug.print("\nidle: {}\n", .{idle});
+// }
+
+// test "del_idle" {
+//     var loop = Self.init(std.testing.allocator);
+//     defer loop.deinit();
+//     var x: u32 = 99;
+//     const ud: *void = @ptrCast(&x);
+//     const idle = try loop.add_idle(&test_idle_cb, ud, 5);
+//     const idle_id = idle.id;
+//     std.debug.print("\n idle: {}\n", .{idle});
+
+//     _ = loop.del_idle(idle_id);
+//     _ = loop.del_idle(idle_id);
+// }
+
+// test "run with idles" {
+//     var loop = Self.init(std.testing.allocator);
+//     defer loop.deinit();
+//     var x: u32 = 99;
+//     const ud: *void = @ptrCast(&x);
+//     _ = try loop.add_idle(&test_idle_cb, ud, 5);
+//     // std.debug.print("\nidle: {}\n", .{idle});
+
+//     try loop.stop_async();
+//     _ = try loop.run();
+// }
+
+fn test_timer_cb(timer: *Timer, ud: *void) void {
+    std.debug.print("test_timer_cb: timer: {}, ud: {}\n", .{ timer.id, ud });
 }
 
-test "run" {
-    std.debug.print("ignore\n", .{});
-    // var loop = Self{};
-    // _ = run(&loop);
-}
+// test "add_timer" {
+//     var loop = Self.init(std.testing.allocator);
+//     defer loop.deinit();
+//     var x: u32 = 99;
+//     const ud: *void = @ptrCast(&x);
+//     const idle = try loop.add_timer(&test_timer_cb, ud, 5, 5);
+//     std.debug.print("\ntimer: {}\n", .{idle});
+// }
 
-test "stop" {
-    var loop = Self.init(std.testing.allocator);
-    defer loop.deinit();
-    _ = stop(&loop);
-}
+// test "del_timer" {
+//     var loop = Self.init(std.testing.allocator);
+//     defer loop.deinit();
+//     var x: u32 = 99;
+//     const ud: *void = @ptrCast(&x);
+//     const timer = try loop.add_timer(&test_timer_cb, ud, 5, 5);
+//     const timer_id = timer.id;
+//     std.debug.print("\n timer: {}\n", .{timer});
 
-test "pause" {
-    var loop = Self.init(std.testing.allocator);
-    defer loop.deinit();
-    _ = pause(&loop);
-}
+//     _ = loop.del_timer(timer_id);
+//     _ = loop.del_timer(timer_id);
+// }
 
-test "resume" {
-    var loop = Self.init(std.testing.allocator);
-    defer loop.deinit();
-    _ = unpause(&loop);
-}
-
-fn test_idle_cb(idle: *Idle, ud: *void) void {
-    std.debug.print("test_cb: idle: {}, ud: {}", .{ idle, ud });
-}
-
-test "add_idle" {
+test "run with timers" {
     var loop = Self.init(std.testing.allocator);
     defer loop.deinit();
     var x: u32 = 99;
     const ud: *void = @ptrCast(&x);
-    const idle = try loop.add_idle(&test_idle_cb, ud, 5);
-    std.debug.print("\nidle: {}\n", .{idle});
-}
-
-test "del_idle" {
-    var loop = Self.init(std.testing.allocator);
-    defer loop.deinit();
-    var x: u32 = 99;
-    const ud: *void = @ptrCast(&x);
-    const idle = try loop.add_idle(&test_idle_cb, ud, 5);
-    const idle_id = idle.id;
-    std.debug.print("\n idle: {}\n", .{idle});
-
-    _ = loop.del_idle(idle_id);
-    _ = loop.del_idle(idle_id);
-}
-
-test "run with idles" {
-    var loop = Self.init(std.testing.allocator);
-    defer loop.deinit();
-    var x: u32 = 99;
-    const ud: *void = @ptrCast(&x);
-    _ = try loop.add_idle(&test_idle_cb, ud, 5);
-    // std.debug.print("\nidle: {}\n", .{idle});
+    _ = try loop.add_timer(&test_timer_cb, ud, 5, 5);
+    // std.debug.print("\ntimer: {}\n", .{timer});
 
     try loop.stop_async();
     _ = try loop.run();
-}
-
-fn test_timer_cb(idle: *Timer, ud: *void) void {
-    std.debug.print("test_cb: idle: {}, ud: {}", .{ idle, ud });
-}
-
-test "add_timer" {
-    var loop = Self.init(std.testing.allocator);
-    defer loop.deinit();
-    var x: u32 = 99;
-    const ud: *void = @ptrCast(&x);
-    const idle = try loop.add_timer(&test_timer_cb, ud, 5, 5);
-    std.debug.print("\ntimer: {}\n", .{idle});
-}
-
-test "del_timer" {
-    var loop = Self.init(std.testing.allocator);
-    defer loop.deinit();
-    var x: u32 = 99;
-    const ud: *void = @ptrCast(&x);
-    const timer = try loop.add_timer(&test_timer_cb, ud, 5, 5);
-    const timer_id = timer.id;
-    std.debug.print("\n timer: {}\n", .{timer});
-
-    _ = loop.del_timer(timer_id);
-    _ = loop.del_timer(timer_id);
 }
--- xkyss-core/src/event/Timer.zig
+++ xkyss-core/src/event/Timer.zig
@@ -6,13 +6,13 @@ pub const CallBack = fn (*Self, *void) void;
 
 // 成员变量
 loop: *Loop = undefined,
-id: u32 = 0,
+id: u32 = undefined,
 timeout: u32 = 0,
-repeat: u32 = 0,
+repeat: u32 = undefined,
 callback: *const CallBack = undefined,
 userdata: *void = undefined,
+next_timeout: u64 = undefined,
 
 // private:
 destroy: bool = true,
 disable: bool = true,
-next_timeout: u32 = 0,
